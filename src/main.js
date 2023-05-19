bd26c63bc7a1d365a6a8bcefc9d86751b0b44ae3
sgocastro <castrosantiagomatias@gmail.com>
2023-05-19 09:26:13 -0300

    WIP: redesign

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import { AppLink, AppLogo, Contact, ContactButton } from "./components"
-import { $, getDateDifference } from "./utils"
+// import { $, getDateDifference } from "./utils"
 
 window.customElements.define("app-logo", AppLogo)
 window.customElements.define("app-link", AppLink)
 window.customElements.define("contact-button", ContactButton)
 window.customElements.define("contact-social-media", Contact)
 
-$("#years-development").innerText = getDateDifference(
-  new Date("08/01/2018")
-).toString()
+// $("#years-development").innerText = getDateDifference(
+//   new Date("08/01/2018")
+// ).toString()
 
-$("#years-in-front-end").innerText = getDateDifference(
-  new Date("12/01/2019")
-).toString()
+// $("#years-in-front-end").innerText = getDateDifference(
+//   new Date("12/01/2019")
+// ).toString()
