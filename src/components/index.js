4ed8d10f67a600203d506f40d640a77fb8f2a70f
sgocastro <castrosantiagomatias@gmail.com>
2023-07-27 08:52:16 -0300

    feat(code): added web components, added new section contact and new styles

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,4 @@
 import AppLogo from "./app-logo"
 import AppLink from "./app-link"
-import ContactButton from "./contact-button"
-import ContactSocialMedia from "./contact-social-media"
 
-export { AppLogo, AppLink, ContactButton, ContactSocialMedia as Contact }
+export { AppLogo, AppLink }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-import { AppLink, AppLogo, Contact, ContactButton } from "./components"
-// import { $, getDateDifference } from "./utils"
+import { AppLink, AppLogo } from "./components"
 
-window.customElements.define("app-logo", AppLogo)
-window.customElements.define("app-link", AppLink)
-window.customElements.define("contact-button", ContactButton)
-window.customElements.define("contact-social-media", Contact)
-
-// $("#years-development").innerText = getDateDifference(
-//   new Date("08/01/2018")
-// ).toString()
-
-// $("#years-in-front-end").innerText = getDateDifference(
-//   new Date("12/01/2019")
-// ).toString()
+customElements.define("app-logo", AppLogo)
+customElements.define("app-link", AppLink)
